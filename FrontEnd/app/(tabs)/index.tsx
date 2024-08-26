5b17e984d84e7bd2fb9ce5cc66a7079082f8e02a
Kyle Lin <linkyle0924@gmail.com>
2024-08-25 22:47:18 -0700

    Add top nav bar

--- FrontEnd/app/(tabs)/index.tsx
+++ FrontEnd/app/(tabs)/index.tsx
@@ -3,19 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style = {styles.screen}>
-      <Text style = {styles.bigCountdown}>10 DAYS</Text>
+      <View style = {styles.navBar}>
+        <Text>All</Text>
+        <Text>Group</Text>
+      </View>
+      <Text style = {styles.bigCountdown}>10:00:00:00</Text>
+      <Text style = {{fontSize: 20}}>Weekend Whip Karaoke</Text>
       <View style = {styles.allCountdownContainers}>
         <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Second Countdown</Text>
+          <Text style = {{color: 'white'}}>Spinjitzu Class: 20:00:00:00</Text>
         </View>
         <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Third Countdown</Text>
+          <Text style = {{color: 'white'}}>Fight Serpentine: 22:00:00:00</Text>
         </View>
         <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Fourth Countdown</Text>
+          <Text style = {{color: 'white'}}>Ghost Whip Karaoke: 27:00:00:00:00</Text>
         </View>
         <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Fifth Countdown</Text>
+          <Text style = {{color: 'white'}}>Pirate Whip Karaoke: 29:00:00:00</Text>
+        </View>
+        <View style = {styles.countdownContainer}>
+          <Text style = {{color: 'white'}}>Expand All</Text>
         </View>
       </View>
     </View>
@@ -26,12 +34,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1, 
     alignItems: 'center',
-    paddingTop: 30,
+    // paddingTop: 50,
     backgroundColor: 'rgb(231, 140, 237)',
   }, 
 
   bigCountdown: {
-    fontSize: 80,
+    fontSize: 60,
+    paddingVertical: 10,
+    paddingTop: 20,
   }, 
 
   countdownContainer: {
@@ -45,6 +55,15 @@ const styles = StyleSheet.create({
   allCountdownContainers: {
     width: '100%',
     gap: 10,
-    paddingTop: 10,
+    paddingTop: 30,
+  },
+
+  navBar: {
+    backgroundColor: 'rgb(199, 191, 198)',
+    height: 60,
+    width: '100%',
+    paddingTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
 });
