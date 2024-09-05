792028298d2140582ac6df6633fa396a0da27e80
Kyle Lin <linkyle0924@gmail.com>
2024-09-05 13:59:58 -0700

    Add navbar functionality

--- FrontEnd/app/(tabs)/index.tsx
+++ FrontEnd/app/(tabs)/index.tsx
@@ -1,35 +1,69 @@
+import React from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
+
+  const [isVisible, setIsVisible] = React.useState(false);
+  const changeVisibility = (isVisible: boolean) => {
+    setIsVisible(isVisible);
+  }
+
   return (
     <View style = {styles.screen}>
       <View style = {styles.navBar}>
-        <Pressable onPress={() => alert('Showing all events')}>
-          <Text>All</Text>
+        <Pressable onPress={() => changeVisibility(false)}>
+          <Text style = {isVisible == false ? styles.selected: {}}>All</Text>
         </Pressable>
-        <Pressable onPress = {() => alert('Dropdown menu appears')}>
-          <Text>Group</Text>
+        <Pressable onPress = {() => changeVisibility(true)}>
+          <Text style = {isVisible == true ? styles.selected : {}}>Group</Text>
         </Pressable>
       </View>
-      <Text style = {styles.bigCountdown}>10:00:00:00</Text>
-      <Text style = {{fontSize: 20}}>Weekend Whip Karaoke</Text>
-      <View style = {styles.allCountdownContainers}>
-        <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Spinjitzu Class: 20:00:00:00</Text>
+      {(isVisible == true) && 
+      (<View style = {styles.group}>
+        <Text style = {styles.bigCountdown}>10:00:00:00</Text>
+        <Text style = {{fontSize: 20}}>Weekend Whip Karaoke</Text>
+        <View style = {styles.allCountdownContainers}>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Spinjitzu Class: 20:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Fight Serpentine: 22:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Ghost Whip Karaoke: 27:00:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Pirate Whip Karaoke: 29:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Expand All</Text>
+          </View>
         </View>
-        <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Fight Serpentine: 22:00:00:00</Text>
+      </View>)}
+
+      {(isVisible == false) && 
+      (<View style = {styles.all}>
+        <Text style = {styles.bigCountdown}>10:00:00:00</Text>
+        <Text style = {{fontSize: 20}}>Weekend Whip Karaoke</Text>
+        <View style = {styles.allCountdownContainers}>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Tae Kwon Do Practice: 15:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Spinjitzu Class: 20:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Fight Serpentine: 22:00:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Weekend Whip Karaoke 2: 24:00:00:00</Text>
+          </View>
+          <View style = {styles.countdownContainer}>
+            <Text style = {{color: 'white'}}>Expand All</Text>
+          </View>
         </View>
-        <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Ghost Whip Karaoke: 27:00:00:00:00</Text>
-        </View>
-        <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Pirate Whip Karaoke: 29:00:00:00</Text>
-        </View>
-        <View style = {styles.countdownContainer}>
-          <Text style = {{color: 'white'}}>Expand All</Text>
-        </View>
-      </View>
+      </View>)}
+
     </View>
   );
 }
@@ -63,11 +97,27 @@ const styles = StyleSheet.create({
   },
 
   navBar: {
-    backgroundColor: 'rgb(199, 191, 198)',
+    backgroundColor: 'rgb(193, 107, 199)',
     height: 60,
     width: '100%',
     paddingTop: 30,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
+  },
+
+  all: {
+    alignItems: 'center',
+    margin: 0,
+    width: '100%'
+  }, 
+
+  group: {
+    alignItems: 'center',
+    margin: 0,
+    width: '100%', 
+  },
+
+  selected: {
+    color: 'white',
   },
 });
